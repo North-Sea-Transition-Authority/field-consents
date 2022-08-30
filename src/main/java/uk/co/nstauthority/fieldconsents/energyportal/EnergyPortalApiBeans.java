e95914f7f755b03c63f7bd7d1256ac2fece56e38
Chris Tasker <chris.tasker@fivium.co.uk>
2022-08-30 16:14:55 +0100

    Pull in Energy Portal API (#8)
    * Pull in Energy Portal API

---
+++ src/main/java/uk/co/nstauthority/fieldconsents/energyportal/EnergyPortalApiBeans.java
@@ -0,0 +1,28 @@
+package uk.co.nstauthority.fieldconsents.energyportal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.co.fivium.energyportalapi.client.EnergyPortal;
+import uk.co.fivium.energyportalapi.client.field.FieldApi;
+import uk.co.fivium.energyportalapi.client.organisation.OrganisationApi;
+
+@Configuration
+public class EnergyPortalApiBeans {
+
+  @Bean
+  EnergyPortal energyPortal(EnergyPortalApiConfig energyPortalApiConfig) {
+    return EnergyPortal.defaultConfiguration(energyPortalApiConfig.url(), energyPortalApiConfig.preSharedKey());
+  }
+
+  @Bean
+  public OrganisationApi organisationApi(EnergyPortal energyPortal) {
+    return new OrganisationApi(energyPortal);
+  }
+
+
+  @Bean
+  public FieldApi fieldApi(EnergyPortal energyPortal) {
+    return new FieldApi(energyPortal);
+  }
+
+}
---
+++ src/main/java/uk/co/nstauthority/fieldconsents/energyportal/EnergyPortalApiConfig.java
@@ -0,0 +1,12 @@
+package uk.co.nstauthority.fieldconsents.energyportal;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "energy-portal-api")
+@Validated
+public record EnergyPortalApiConfig(
+    @NotNull String url,
+    @NotNull String preSharedKey
+) {}
