061777c5cb13d5f778c1781b4618c895b55e3724
Harry Devane <harry.devane@fivium.co.uk>
2024-06-03 10:10:12 +0100

    Update months tables timestamptz column types to date (#507)

---
+++ src/main/resources/db/migration/V10.2__update_months_tables_timestamptz_column_types_to_date.sql
@@ -0,0 +1,11 @@
+ALTER TABLE flare_short_term_months
+  ALTER start_date TYPE DATE,
+  ALTER end_date TYPE DATE;
+
+ALTER TABLE short_term_production_months
+  ALTER start_date TYPE DATE,
+  ALTER end_date TYPE DATE;
+
+ALTER TABLE vent_short_term_months
+  ALTER start_date TYPE DATE,
+  ALTER end_date TYPE DATE;
