3f542931f00ddc93707d69bbcaf333fd8714a545
Chris Tasker <chris.tasker@fivium.co.uk>
2024-06-12 13:35:53 +0100

    Use MDC CorrelationID as default (#521)

--- src/main/java/uk/co/nstauthority/fieldconsents/energyportal/api/EnergyPortalApiBeans.java
+++ src/main/java/uk/co/nstauthority/fieldconsents/energyportal/api/EnergyPortalApiBeans.java
@@ -3,18 +3,25 @@ package uk.co.nstauthority.fieldconsents.energyportal.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.co.fivium.energyportalapi.client.EnergyPortal;
+import uk.co.fivium.energyportalapi.client.LogCorrelationId;
 import uk.co.fivium.energyportalapi.client.field.FieldApi;
 import uk.co.fivium.energyportalapi.client.organisation.OrganisationApi;
 import uk.co.fivium.energyportalapi.client.pets.PetsApplicationApi;
 import uk.co.fivium.energyportalapi.client.terminal.TerminalApi;
 import uk.co.fivium.energyportalapi.client.user.UserApi;
+import uk.co.nstauthority.fieldconsents.correlationid.CorrelationIdUtil;
 
 @Configuration
 public class EnergyPortalApiBeans {
 
   @Bean
   EnergyPortal energyPortal(EnergyPortalApiConfig energyPortalApiConfig) {
-    return EnergyPortal.defaultConfiguration(energyPortalApiConfig.url(), energyPortalApiConfig.preSharedKey());
+    return EnergyPortal.customConfiguration(
+        energyPortalApiConfig.url(),
+        energyPortalApiConfig.preSharedKey(),
+        EnergyPortal.DEFAULT_REQUEST_TIMEOUT_SECONDS,
+        () -> new LogCorrelationId(CorrelationIdUtil.getCorrelationIdFromMdc())
+        );
   }
 
   @Bean
