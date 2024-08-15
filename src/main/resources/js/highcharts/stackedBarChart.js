840842d04a6d315f321196e2b1b2f7276efe314d
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2024-08-15 16:03:21 +0100

    FCS-904: do highcharts image generation on the client

--- src/main/resources/js/highcharts/stackedBarChart.js
+++ src/main/resources/js/highcharts/stackedBarChart.js
@@ -1,9 +1,11 @@
 import Highcharts from "highcharts";
 import highchartsAccessibility from "highcharts/modules/accessibility";
 import highchartsExporting from "highcharts/modules/exporting"
+import highCartsOfflineExporting from "highcharts/modules/offline-exporting"
 
 highchartsAccessibility(Highcharts);
 highchartsExporting(Highcharts);
+highCartsOfflineExporting(Highcharts);
 
 export default class StackedBarChart {
 
@@ -19,6 +21,11 @@ export default class StackedBarChart {
       chart: {
         type: 'column',
         styledMode: true,
+      },
+      exporting: {
+        // Older browsers may not support exporting on the client so they'll fall back to using the Highcharts servers.
+        // https://www.highcharts.com/docs/export-module/client-side-export
+        fallbackToExportServer: false
       },
       credits: {
         enabled: false,
