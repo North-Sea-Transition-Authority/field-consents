77c1b68645d3de09aa17d510d1c39a8983b56edc
joshuabd-fivium <joshua.beresford-davis@fivium.co.uk>
2024-08-20 09:44:24 +0100

    FCS-852: Add announce text to select all / select none actions on bulk case actions page (#600)
    * FCS-852: Add announcements.
    
    * FCS-852: Updated deselection text.

--- src/main/resources/js/selectableResultsAndActionsContainer.js
+++ src/main/resources/js/selectableResultsAndActionsContainer.js
@@ -1,14 +1,20 @@
+import announce from "../templates/fds/js/accessibility";
+
 export default class SelectableResultsAndActionsContainer {
 
   constructor(module) {
     const checkboxes = module.querySelectorAll("ol.fds-result-list input[type='checkbox']")
 
-    module.querySelector(".selectAllResults").addEventListener("click", () =>
-      checkboxes.forEach(checkbox => this.setCheckboxValue(checkbox, true))
+    module.querySelector(".selectAllResults").addEventListener("click", () => {
+        checkboxes.forEach(checkbox => this.setCheckboxValue(checkbox, true));
+        announce(`All applications selected (${checkboxes.length} in total)`);
+      }
     );
 
-    module.querySelector(".deSelectAllResults").addEventListener("click", () =>
-      checkboxes.forEach(checkbox => this.setCheckboxValue(checkbox, false))
+    module.querySelector(".deSelectAllResults").addEventListener("click", () => {
+        checkboxes.forEach(checkbox => this.setCheckboxValue(checkbox, false));
+        announce(`No applications selected`);
+      }
     );
   }
 
