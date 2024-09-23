57268bf102272cd554c8f38fcad6148a7794d4f3
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2024-09-23 10:25:05 +0100

    Revert "add limit to jooq query (#641)" (#644)

--- src/main/java/uk/co/nstauthority/fieldconsents/search/SearchController.java
+++ src/main/java/uk/co/nstauthority/fieldconsents/search/SearchController.java
@@ -39,8 +39,6 @@ public class SearchController {
 
   public static final String SEARCH_RESULT_ITEMS = "searchResultItems";
 
-  public static final int SEARCH_RESULT_RENDER_LIMIT = 300;
-
   private final TeamService teamService;
 
   private final SearchService searchService;
@@ -48,6 +46,8 @@ public class SearchController {
   private final ApplicationDataFilterFormService applicationDataFilterFormService;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SearchController.class);
+
+  private static final int SEARCH_RESULT_RENDER_LIMIT = 300;
 
   SearchController(TeamService teamService,
                    SearchService searchService,
