91dfd3fcd5fa5a819f00f27551f077e20c9571b7
gogrady <george.ogrady@fivium.co.uk>
2025-09-02 09:43:17 +0100

    EPUO-391: Tell EPAS about roles for a team type via EPQM (#733)

---
+++ src/main/java/uk/co/nstauthority/fieldconsents/teams/EnergyPortalTeamTypeRoleService.java
@@ -0,0 +1,40 @@
+package uk.co.nstauthority.fieldconsents.teams;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import uk.co.fivium.energyportal.serviceproviders.epmq.messages.ServiceProviderTeamTypeRoleDto;
+import uk.co.fivium.energyportal.starter.serviceproviders.EnergyPortalServiceProviderTeamTypeRoleService;
+
+@Profile("use-service-access-request")
+@Service
+public class EnergyPortalTeamTypeRoleService {
+
+  private final EnergyPortalServiceProviderTeamTypeRoleService serviceProviderTeamTypeRoleService;
+
+  EnergyPortalTeamTypeRoleService(EnergyPortalServiceProviderTeamTypeRoleService serviceProviderTeamTypeRoleService) {
+    this.serviceProviderTeamTypeRoleService = serviceProviderTeamTypeRoleService;
+  }
+
+  @EventListener(classes = ApplicationReadyEvent.class)
+  void publishRolesForTeamTypeMessage() {
+    for (var teamType : TeamType.values()) {
+      var serviceRoleDtos = teamType.getAllowedRoles()
+          .stream()
+          .map(role -> new ServiceProviderTeamTypeRoleDto(
+                  role.name(),
+                  role.getDisplayName(),
+                  role.getDescription(),
+                  role == Role.ACCESS_MANAGER,
+                  Arrays.stream(Role.values()).toList().indexOf(role)
+              )
+          ).collect(Collectors.toSet());
+
+      var isRegulatorTeamType = teamType.equals(TeamType.REGULATOR) || teamType.equals(TeamType.CONSULTEE);
+      serviceProviderTeamTypeRoleService.publishRolesForTeamType(serviceRoleDtos, teamType.name(), isRegulatorTeamType);
+    }
+  }
+}
---
+++ src/test/java/uk/co/nstauthority/fieldconsents/teams/EnergyPortalTeamTypeRoleServiceTest.java
@@ -0,0 +1,74 @@
+package uk.co.nstauthority.fieldconsents.teams;
+
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.co.fivium.energyportal.serviceproviders.epmq.messages.ServiceProviderTeamTypeRoleDto;
+import uk.co.fivium.energyportal.starter.serviceproviders.EnergyPortalServiceProviderTeamTypeRoleService;
+
+@ExtendWith(MockitoExtension.class)
+class EnergyPortalTeamTypeRoleServiceTest {
+
+  @Mock
+  private EnergyPortalServiceProviderTeamTypeRoleService serviceProviderTeamTypeRoleService;
+
+  @InjectMocks
+  private EnergyPortalTeamTypeRoleService energyPortalTeamTypeRoleService;
+
+  private static final List<Role> ORDERED_ROLES = Arrays.stream(Role.values()).toList();
+
+  @Test
+  void publishRolesForTeamTypeMessage() {
+
+    var regulatorServiceRoleDtos = Set.of(
+        createServiceRoleDto(Role.ACCESS_MANAGER, true),
+        createServiceRoleDto(Role.INDUSTRY_ACCESS_MANAGER, false),
+        createServiceRoleDto(Role.DOCUMENT_TEMPLATE_MANAGER, false),
+        createServiceRoleDto(Role.CASE_OFFICER, false),
+        createServiceRoleDto(Role.CASE_MANAGER, false),
+        createServiceRoleDto(Role.CONSENTS_AND_AUTHORISATIONS_MANAGER, false),
+        createServiceRoleDto(Role.TECHNICAL_REVIEWER, false),
+        createServiceRoleDto(Role.VIEWER, false)
+    );
+
+    var consulteeServiceRoleDtos = Set.of(
+        createServiceRoleDto(Role.ACCESS_MANAGER, true),
+        createServiceRoleDto(Role.ALLOCATOR, false),
+        createServiceRoleDto(Role.RESPONDER, false),
+        createServiceRoleDto(Role.VIEWER, false)
+    );
+    var organisationServiceRoleDtos = Set.of(
+        createServiceRoleDto(Role.ACCESS_MANAGER, true),
+        createServiceRoleDto(Role.VIEWER, false),
+        createServiceRoleDto(Role.EDITOR, false),
+        createServiceRoleDto(Role.SUBMITTER, false),
+        createServiceRoleDto(Role.CREATOR, false),
+        createServiceRoleDto(Role.FINANCE_ADMINISTRATOR, false),
+        createServiceRoleDto(Role.CONSENT_RECIPIENT, false)
+    );
+
+    energyPortalTeamTypeRoleService.publishRolesForTeamTypeMessage();
+
+    verify(serviceProviderTeamTypeRoleService).publishRolesForTeamType(regulatorServiceRoleDtos, TeamType.REGULATOR.name(), true);
+    verify(serviceProviderTeamTypeRoleService).publishRolesForTeamType(consulteeServiceRoleDtos, TeamType.CONSULTEE.name(), true);
+    verify(serviceProviderTeamTypeRoleService).publishRolesForTeamType(organisationServiceRoleDtos, TeamType.INDUSTRY.name(),
+        false);
+  }
+
+  private ServiceProviderTeamTypeRoleDto createServiceRoleDto(Role role, boolean isAssessManager) {
+    return new ServiceProviderTeamTypeRoleDto(
+        role.name(),
+        role.getDisplayName(),
+        role.getDescription(),
+        isAssessManager,
+        ORDERED_ROLES.indexOf(role)
+    );
+  }
+}
