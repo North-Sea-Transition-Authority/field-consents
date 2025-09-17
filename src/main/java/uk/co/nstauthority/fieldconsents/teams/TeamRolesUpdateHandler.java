108109673fdbdd716f385981c81ec157d2afa92e
gogrady <george.ogrady@fivium.co.uk>
2025-09-17 14:31:31 +0100

    EPUO-401: Listen and handle EPMQ messages from EPAS regard team role updates.

---
+++ src/main/java/uk/co/nstauthority/fieldconsents/teams/TeamRolesUpdateHandler.java
@@ -0,0 +1,64 @@
+package uk.co.nstauthority.fieldconsents.teams;
+
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import uk.co.fivium.energyportal.serviceproviders.epmq.messages.ServiceProviderTeamRolesEpasMessage;
+import uk.co.fivium.energyportal.starter.configuration.EnergyPortalAccountsConfigurationProperties;
+import uk.co.fivium.energyportal.starter.serviceproviders.EnergyPortalTeamRolesUpdateHandler;
+import uk.co.nstauthority.fieldconsents.audit.AuditRevisionUtil;
+import uk.co.nstauthority.fieldconsents.authentication.ServiceUserDetail;
+import uk.co.nstauthority.fieldconsents.energyportal.WebUserAccountId;
+import uk.co.nstauthority.fieldconsents.energyportal.user.EnergyPortalUserService;
+import uk.co.nstauthority.fieldconsents.teams.management.TeamManagementService;
+
+@Profile("use-service-access-request")
+@Component
+public class TeamRolesUpdateHandler implements EnergyPortalTeamRolesUpdateHandler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TeamRolesUpdateHandler.class);
+
+  private final EnergyPortalUserService energyPortalUserService;
+  private final TeamManagementService teamManagementService;
+  private final String serviceName;
+
+  TeamRolesUpdateHandler(EnergyPortalUserService energyPortalUserService,
+                         TeamManagementService teamManagementService,
+                         EnergyPortalAccountsConfigurationProperties energyPortalAccountsConfigurationProperties
+  ) {
+    this.energyPortalUserService = energyPortalUserService;
+    this.teamManagementService = teamManagementService;
+    this.serviceName = energyPortalAccountsConfigurationProperties.serviceName();
+  }
+
+  @Override
+  public void accept(ServiceProviderTeamRolesEpasMessage serviceProviderTeamRolesEpasMessage) {
+    if (!serviceName.equals(serviceProviderTeamRolesEpasMessage.getService())) {
+      return;
+    }
+
+    var serviceProviderUserTeamRolesDto = serviceProviderTeamRolesEpasMessage.getServiceProviderUserTeamRolesDto();
+    var optionalTeam = teamManagementService.getTeam(UUID.fromString(serviceProviderUserTeamRolesDto.teamId()));
+
+    if (optionalTeam.isEmpty()) {
+      LOGGER.error("Team not found for id: {}, when updating team_roles from epas team roles update message. correlationId: {}",
+          serviceProviderUserTeamRolesDto.teamId(),
+          serviceProviderTeamRolesEpasMessage.getCorrelationId()
+      );
+      return;
+    }
+
+    var invokingUser = energyPortalUserService
+        .getByWuaId(WebUserAccountId.from(serviceProviderTeamRolesEpasMessage.getDeciderWuaId()));
+
+    AuditRevisionUtil.withFallbackAuditUser(
+        ServiceUserDetail.from(invokingUser),
+        () -> teamManagementService.setUserTeamRoles(
+            serviceProviderUserTeamRolesDto.wuaId(),
+            optionalTeam.get(),
+            serviceProviderUserTeamRolesDto.roles().stream().map(Role::valueOf).toList())
+    );
+  }
+}
